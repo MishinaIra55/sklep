446efe0c504c112e927355f9e7a23c147fdcda9d
MishynaIra <gortenzia1987@gmail.com>
2023-08-23 12:06:46 +0300

    1.верстаю header и пишу classname обычным способом

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './index.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return<div className='wrapper'>
+    <header>
+      <div className='headerLeft'>
+        <svg/>
+        <div className='headerInfo'>
+          <h3>React Cookies</h3>
+          <p> Магазин вкусняшек</p>
+        </div>
+      </div>
+
+      <ul className='headerRight'>
+        <li>
+          <svg/>
+          <span> 2000грн</span>
+        </li>
+        <li>
+          <svg/>
+        </li>
+      </ul>
+    </header>
+  </div>
 }
 
 export default App;
