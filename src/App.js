4e2797b24bf0f6517f544807d0ebce185ae97a8a
MishynaIra <gortenzia1987@gmail.com>
2023-08-25 16:26:01 +0300

    22.создала компонент Drawer и перенесла разметку html в компонент

--- src/App.js
+++ src/App.js
@@ -2,55 +2,13 @@
 import './index.scss';
 import Card from './components/Card';
 import Header from "./components/Header";
+import Drawer from "./components/Drawer";
 
 function App() {
   return (
       <div className='wrapper'>
   <div  style={{display: 'none'}} className='overlay'>
-    <div className='drawer'>
-    <h2 className='mb-30'>Кошик
-      <img className='btnClose' src='/images/btnclose.svg'  alt='btnclose'/>
-    </h2>
-
-     <div className='items'>
-       <div className='cartItem'>
-         <div style={{backgroundImage:'url(/images/sneakers/1.jpg)'}} className='cartItemImg'> </div>
-
-         <div className='cartInfo'>
-           <p className='cartText'>Мужские Кроссовки Nike Air Max 270</p>
-           <b>2000 грн</b>
-         </div>
-         <img className='btnClose' src='/images/btnclose.svg'  alt='btnclose'/>
-       </div>
-       <div className='cartItem'>
-         <div style={{backgroundImage:'url(/images/sneakers/1.jpg)'}} className='cartItemImg'> </div>
-
-         <div className='cartInfo'>
-           <p className='cartText'>Мужские Кроссовки Nike Air Max 270</p>
-           <b>2000 грн</b>
-         </div>
-         <img className='btnClose' src='/images/btnclose.svg'  alt='btnclose'/>
-       </div>
-
-
-     </div>
-
-     <div className='cartTotalInfo'>
-       <ul >
-         <li >
-           <span>Разом:</span>
-           <div></div>
-           <b> 20000 грн </b>
-         </li>
-         <li >
-           <span>Податок 5%:</span>
-           <div></div>
-           <b>2000 грн</b>
-         </li>
-       </ul>
-       <button className='greenButton'>Замовити замовлення <img src='/images/arrow.svg' alt='arrow'/></button>
-     </div>
-    </div>
+    <Drawer/>
   </div>
     <Header/>
     <div className='content'>
---
+++ src/components/Drawer.js
@@ -0,0 +1,50 @@
+function Drawer () {
+    return (
+        <div className='drawer'>
+            <h2 className='mb-30'>Кошик
+                <img className='btnClose' src='/images/btnclose.svg'  alt='btnclose'/>
+            </h2>
+
+            <div className='items'>
+                <div className='cartItem'>
+                    <div style={{backgroundImage:'url(/images/sneakers/1.jpg)'}} className='cartItemImg'> </div>
+
+                    <div className='cartInfo'>
+                        <p className='cartText'>Мужские Кроссовки Nike Air Max 270</p>
+                        <b>2000 грн</b>
+                    </div>
+                    <img className='btnClose' src='/images/btnclose.svg'  alt='btnclose'/>
+                </div>
+                <div className='cartItem'>
+                    <div style={{backgroundImage:'url(/images/sneakers/1.jpg)'}} className='cartItemImg'> </div>
+
+                    <div className='cartInfo'>
+                        <p className='cartText'>Мужские Кроссовки Nike Air Max 270</p>
+                        <b>2000 грн</b>
+                    </div>
+                    <img className='btnClose' src='/images/btnclose.svg'  alt='btnclose'/>
+                </div>
+
+
+            </div>
+
+            <div className='cartTotalInfo'>
+                <ul >
+                    <li >
+                        <span>Разом:</span>
+                        <div></div>
+                        <b> 20000 грн </b>
+                    </li>
+                    <li >
+                        <span>Податок 5%:</span>
+                        <div></div>
+                        <b>2000 грн</b>
+                    </li>
+                </ul>
+                <button className='greenButton'>Замовити замовлення <img src='/images/arrow.svg' alt='arrow'/></button>
+            </div>
+        </div>
+    )
+}
+
+export default Drawer;
