02022e56bb8ebf6710868f99eaab4ae780015c06
MishynaIra <gortenzia1987@gmail.com>
2023-09-04 13:35:22 +0300

    56.с помощью условного рендеринга отображаю или пустую корзину или корзину с карточками товара

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -5,32 +5,35 @@ function Drawer ({onRemove, onClose,items= []}) {
             <h2 className='mb-30'>Кошик
                 <img onClick={onClose}className='btnClose' src='/images/btnclose.svg'  alt='btnclose'/>
             </h2>
+            {
+                items.length > 0 ? <div className='items'>
+                    {
+                        items.map((object,index) => (
+                            <div key={index} className='cartItem'>
+                                <div style={{ backgroundImage:`url(${object.image})`}} className='cartItemImg'> </div>
 
-            <div className='cartEmpty'>
-                <img className='imageEmpty' width={120} height={120} src='/images/empty-cart.jpg' alt='empty cart'/>
-                <h2 className='mb-20'>Кошик пустий</h2>
-                <p className='opacity-6 mb-20'>Добавте хоть один товар, щоб зробити замовлення</p>
-                <button className='greenEmptyCart' >
-                    <img src='/images/arrow-cart.png' alt='arrow'/>
-                    Повернутися назад
-                </button>
-            </div>
+                                <div className='cartInfo'>
+                                    <p className='cartText'>{object.title}</p>
+                                    <b>{object.price} грн</b>
+                                </div>
+                                <img onClick={()=> onRemove(object.id)} className='btnClose' src='/images/btnclose.svg'  alt='btnclose'/>
+                            </div>
+                        ))
+                    }
+                </div> :
+                    <div className='cartEmpty'>
+                    <img className='imageEmpty' width={120} height={120} src='/images/empty-cart.jpg' alt='empty cart'/>
+                    <h2 className='mb-20'>Кошик пустий</h2>
+                    <p className='opacity-6 mb-20'>Добавте хоть один товар, щоб зробити замовлення</p>
+                    <button className='greenEmptyCart' >
+                        <img src='/images/arrow-cart.png' alt='arrow'/>
+                        Повернутися назад
+                    </button>
+                </div>
+            }
 
-            <div className='items'>
-                {
-                    items.map((object,index) => (
-                    <div key={index} className='cartItem'>
-                    <div style={{ backgroundImage:`url(${object.image})`}} className='cartItemImg'> </div>
 
-                    <div className='cartInfo'>
-                    <p className='cartText'>{object.title}</p>
-                    <b>{object.price} грн</b>
-                    </div>
-                    <img onClick={()=> onRemove(object.id)} className='btnClose' src='/images/btnclose.svg'  alt='btnclose'/>
-                    </div>
-                    ))
-                }
-            </div>
+
 
             <div className='cartTotalInfo'>
                 <ul >
