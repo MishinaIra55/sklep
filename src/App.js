e100036dcbf9a6eef19df1c8bbe607edeb4c36c2
MishynaIra <gortenzia1987@gmail.com>
2023-09-05 12:02:47 +0300

    59.при удаление товара карточки в корзине удаляляется товар и на mockapi / axios.delete

--- src/App.js
+++ src/App.js
@@ -34,8 +34,7 @@ function App() {
 
     //метод удаления карточки товара
     const onRemoveItem = (id) => {
-
-        // axios.post(`https://64edba671f8721827141a748.mockapi.io/cart/${id}`);
+        axios.delete(`https://64edba671f8721827141a748.mockapi.io/cart/${id}`);
          setCartItems((prev) => prev.filter((item) => item.id !== id));
     };
 
