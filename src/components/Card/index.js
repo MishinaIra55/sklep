592bf04ff5443fdcf4784158e47c25341199af6d
MishynaIra <gortenzia1987@gmail.com>
2023-09-11 12:43:36 +0300

    78.loading создала для скелетона или рендерю либо заглушки для карточек или сами карточки

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,8 @@
 import styles from './Card.module.scss';
 import {useState} from "react";
+import ContentLoader from "react-content-loader"
 
-function Card  ({ id, image, title, price, onPlus, onFavorite, liked, added=false}) {
+function Card  ({ id, image, title, price, onPlus, onFavorite, liked, added=false,loading = false}) {
     const [isAdded, setIsAdded] = useState(added);
     // const [isFavorite, setIsFavorite] = useState(liked);
 
@@ -17,30 +18,50 @@ function Card  ({ id, image, title, price, onPlus, onFavorite, liked, added=fals
 
     return (
         <div className={styles.card}>
-            <div className={styles.favorite}
-                  // onClick={onClickFavorite}
+            {loading ? <ContentLoader
+                speed={2}
+                width={155}
+                height={265}
+                viewBox="0 0 155 265"
+                backgroundColor="#f3f3f3"
+                foregroundColor="#ecebeb"
+
             >
-                {/*<img src={isFavorite ? '/images/liked.svg' : '/images/unliked.png'}  width='30'  height='30' alt='unliked'/>*/}
-            </div>
-            <img src={image} width={133}  height={112} alt='sneakers'/>
-            <h5>{title}</h5>
-            <div className={styles.cardBlock}>
-                <div className={styles.cardText}>
-                    <span>Ціна:</span>
-                    <b>{price}</b>
+                <rect x="1" y="-5" rx="10" ry="10" width="155" height="155" />
+                <rect x="0" y="157" rx="5" ry="5" width="155" height="15" />
+                <rect x="2" y="186" rx="5" ry="5" width="100" height="15" />
+                <rect x="0" y="220" rx="5" ry="5" width="80" height="24" />
+                <rect x="110" y="217" rx="10" ry="10" width="32" height="32" />
+            </ContentLoader> : <>
+                <div className={styles.favorite}
+                    // onClick={onClickFavorite}
+                >
+                    {/*<img src={isFavorite ? '/images/liked.svg' : '/images/unliked.png'}  width='30'  height='30' alt='unliked'/>*/}
                 </div>
-                <button className={styles.plus} onClick={onClickPlus}>
-                    <img
-                        src={isAdded ? '/images/checked.svg' : '/images/plus1.svg'}
-                        alt='plus1'
-                        width='11'
-                        height='11'
-                        onClick={onClickPlus}
-                    />
-                </button>
-            </div>
+                <img src={image} width={133}  height={112} alt='sneakers'/>
+                <h5>{title}</h5>
+                <div className={styles.cardBlock}>
+                    <div className={styles.cardText}>
+                        <span>Ціна:</span>
+                        <b>{price}</b>
+                    </div>
+                    <button className={styles.plus} onClick={onClickPlus}>
+                        <img
+                            src={isAdded ? '/images/checked.svg' : '/images/plus1.svg'}
+                            alt='plus1'
+                            width='11'
+                            height='11'
+                            onClick={onClickPlus}
+                        />
+                    </button>
+                </div>
+            </>}
+
+
+
         </div>
     )
 }
 
 export default Card;
+
