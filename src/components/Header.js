e6f3794e1287c9332bbbcd6f41dab97f76b3620e
MishynaIra <gortenzia1987@gmail.com>
2023-09-25 11:23:24 +0300

    95. Делаю общую сумму цены корсовок. вытаскиваю cartItems из контекста

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import {Link} from "react-router-dom";
+import {useContext} from "react";
+import AppContext from "../context";
 
 function Header (props) {
+    const {cartItems} = useContext(AppContext);
     return (
         <header>
             <Link to="/">
